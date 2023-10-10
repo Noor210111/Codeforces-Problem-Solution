732c3b6b49ce9e6a80675548d3d50bd35e05a1cc
Abu Mohammad Noor <noor.cse.pust@gmail.com>
2023-10-10 19:31:51 +0600

    Add files via upload

---
+++ A/1421A - XORwice.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t; cin >> t;
+    while(t--)
+    {
+        int a, b; cin >> a >> b;
+
+        int d = a ^ b;
+
+        cout << d << endl;
+    }
+
+    return 0;
+}
