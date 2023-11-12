d82d95a8238cfebc571fcdbb39e544edac2533d8
Abu Mohammad Noor <noor.cse.pust@gmail.com>
2023-11-12 21:58:53 +0600

    Add files via upload

---
+++ A/1894A - Secret Sport.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    int n; cin >> n;
+
+    string s; cin >> s;
+
+    cout << s.back() << endl;
+}
+
+int main()
+{
+    int t; cin >> t;
+    while(t--)
+        solve();
+
+    return 0;
+}
